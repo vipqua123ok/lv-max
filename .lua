5b930045ee99dc1c36aa988fc3f5fb46a4c86611
vipqua123ok <nnsads005@gmail.com>
2024-10-27 08:03:54 +0700

    .lua

---
+++ .lua
@@ -0,0 +1,13 @@
+getgenv().Configs = {
+    ["Team"] = "Pirates",
+    ["FPS Boost"] = true,
+    ["Farm Boss Drops"] = {
+        ["Enable"] = false,
+        ["When x2 Exp Expired"] = false
+    },
+    ["Hop Player Near"] = false,
+    ["Soul Guitar"] = false,
+    ["Find Fruit"] = true, -- Will find 1m+ fruit to unlock swan door to access third sea
+    ["Cursed Dual Katana"] = false
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/refs/heads/main/kaitun.lua"))()
